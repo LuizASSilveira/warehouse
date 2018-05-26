7dba3d03aebfa0c8a7efd1076dca6168a584e922
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-05-26 18:30:37 -0300

    nome

--- src/componentes/login.js
+++ src/componentes/login.js
@@ -36,7 +36,7 @@ export default class Login extends Component {
     render(){
         return (
             <div className="login-box">
-            <h1 className="header-logo">Instalura</h1>
+            <h1 className="header-logo">WareHouse</h1>
             <span>{this.state.msg}</span>
                 <form onSubmit={this.envia.bind(this)}>
                     <input type="text"      ref={(input) => this.login = input}/>
