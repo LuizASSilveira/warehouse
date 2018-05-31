ad10d29556bcb070a79db83148a8fb52fec7adbb
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-05-31 20:50:16 -0300

    row select

--- src/componentes/table.js
+++ src/componentes/table.js
@@ -3,18 +3,13 @@ import './css/table.css'
 import {BootstrapTable, TableHeaderColumn} from '../../node_modules/react-bootstrap-table';
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-var selectRowProp = {
-  mode: 'checkbox'
-};
-
-
+var lista = []
 class Table extends Component {
     constructor(){
         super()
         this.state = {products:[]}
     }
 
-     
     componentDidMount(){
         fetch('https://raw.githubusercontent.com/LuizASSilveira/pi-almoxarifado/master/listSolicitacao.json')
         .then(response => response.json())
@@ -24,10 +19,26 @@ class Table extends Component {
     }
     
     render() {
+      function onRowSelect(row, isSelected) {
+        if (isSelected) {
+          lista.push(row.id);
+          console.log(lista)
+          return false;
+        }
+      }
+
+      var selectRowProp = {
+        mode:    'checkbox',
+        bgColor: 'pink', 
+        hideSelectColumn: true, 
+        clickToSelect: true,
+        onSelect: onRowSelect
+      };
+
       return (
         <div id ="table">
           <BootstrapTable
-            data = { this.state.products } selectRow={ selectRowProp }
+            data = { this.state.products } selectRow={ selectRowProp } 
             pagination
             >
             <TableHeaderColumn dataField='id' isKey>  Product ID    </TableHeaderColumn>
