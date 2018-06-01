c06ff441c7026bd76809992d0f53e2662e8ee9dc
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-05-31 22:36:03 -0300

    table actions

--- src/Requisicao/criar.js
+++ src/Requisicao/criar.js
@@ -1,54 +1,19 @@
 import React, { Component } from 'react';
 import Nav from '../componentes/navbarAdm';
-import Teste from '../componentes/confirmModal';
-import {Button, Form}  from 'reactstrap';
+
+import {Form}  from 'reactstrap';
 import {Table} from '../componentes/table';
 import InputG from '../componentes/inputGenerico';
 
 export default class Criar extends Component {
-	constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
-    this.toggle= this.toggle.bind(this)
-    this.funcCancel= this.funcCancel.bind(this)
-    this.funcConfirm= this.funcConfirm.bind(this)
- 	}
-
-  	funcConfirm(){
-		this.toggle()
-  	}
-
-  	funcCancel(){
-  		this.toggle()
-	}
-
- 	toggle(){
-
-  		this.setState({
-  			modal: !this.state.modal
-  		})
-  	}
-
 
     render(){
-		let modal = ""
-
-  		if(this.state.modal){
-  		modal = <Teste onCancel={this.funcCancel} onConfirm={this.funcConfirm} toggle={true} mensagem={'Deseja confirmar?'}/>
-
-  		// this.toggle()
-  		// setTimeout( this.toggle(), 100)
-  		}
-
 
         return(
             <div>
                 <Nav />
-                <Table  />
-                <Button color="danger" onClick={this.toggle}>Criar Requisição</Button>
-				{modal}
+                <Table buttonName={'Criar Requisicao'} />
+                
 				<Form>
 					<InputG classname={'valid'} label={'もしもし'} placeholder={'何だこれ'} type={'textarea'} formtext={'なるほど'}/>
 					<InputG classname={'valid'} label={'もしもし'} placeholder={'何だこれ'} type={'number'} formtext={'なるほど'}/>
--- src/componentes/table.js
+++ src/componentes/table.js
@@ -2,49 +2,69 @@ import React, { Component } from 'react';
 import './css/table.css'
 import {BootstrapTable, TableHeaderColumn} from '../../node_modules/react-bootstrap-table';
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import {Button}  from 'reactstrap';
+import Teste from '../componentes/confirmModal';
 
-var lista = []
+var selected= []
+function onRowSelect(row, isSelected, e) {
+  if(isSelected){
+    selected.push(row.id)
+  }else{
+    selected.pop(row.id)
+  }
+  console.log(selected)
+}
 class Table extends Component {
     constructor(){
         super()
-        this.state = {products:[]}
+        this.state = {products:[],modal: false}
+        this.toggle= this.toggle.bind(this)
+        this.funcCancel= this.funcCancel.bind(this)
+        this.funcConfirm= this.funcConfirm.bind(this)
     }
-
     componentDidMount(){
         fetch('https://raw.githubusercontent.com/LuizASSilveira/pi-almoxarifado/master/listSolicitacao.json')
         .then(response => response.json())
         .then(product => {         
         this.setState({products:product});
         });      
-    }
-    
-    render() {
-      function onRowSelect(row, isSelected) {
-        if (isSelected) {
-          lista.push(row.id);
-          console.log(lista)
-          return false;
-        }
+    }    
+  	funcConfirm(){
+      console.log("salvo")
       }
-
+    funcCancel(){
+      this.toggle()
+    }
+    toggle(){
+      if(selected.length !== 0){
+        this.setState({
+          modal: !this.state.modal
+        })
+      }
+    }
+    render(){
+      let modal = ""
+  		if(this.state.modal){
+  		modal = <Teste onCancel={this.funcCancel} onConfirm={this.funcConfirm} toggle={true} mensagem={'Deseja confirmar?'}/>
+  		}
       var selectRowProp = {
-        mode:    'checkbox',
-        bgColor: 'pink', 
-        hideSelectColumn: true, 
         clickToSelect: true,
+        mode: 'checkbox',
+        bgColor: 'pink', 
         onSelect: onRowSelect
       };
-
       return (
         <div id ="table">
           <BootstrapTable
-            data = { this.state.products } selectRow={ selectRowProp } 
+            data = { this.state.products } selectRow={ selectRowProp }
             pagination
             >
-            <TableHeaderColumn dataField='id' isKey>  Product ID    </TableHeaderColumn>
+            <TableHeaderColumn dataField='id' isKey>  ID    </TableHeaderColumn>
             <TableHeaderColumn dataField='descricao'> Product Name  </TableHeaderColumn>
             <TableHeaderColumn dataField='status'>    Product Price </TableHeaderColumn>
           </BootstrapTable>
+          <Button color="danger" onClick={this.toggle}>{this.props.buttonName}</Button>
+				  {modal}
         </div>
       );
     }
