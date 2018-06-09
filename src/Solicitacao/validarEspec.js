082152a68ce45a88bf03c1709b9d58fb56661d0b
caionakai <kaioesperto@hotmail.com>
2018-06-09 14:57:35 -0300

    botao do siorg

--- src/Solicitacao/validarEspec.js
+++ src/Solicitacao/validarEspec.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Nav      from '../componentes/navbarAdm';
 import '../componentes/css/input.css'
-import {  Input } from 'reactstrap';
+import {  Input, Button, Label, FormGroup,  Modal, ModalHeader, ModalBody, ModalFooter  } from 'reactstrap';
 import InputG from "../componentes/inputGenerico";
 import OrcamentosTable from "../componentes/table/orcamentos";
-import '../componentes/css/validSolTable.css'
+import TableSiorg from "../componentes/table/siorgTable";
 
 
 export default class validEspec extends Component { 
@@ -83,19 +83,38 @@ componentDidMount() {
          <div>
            <Nav isadm = {true} />
            <h3>Validar Solicitação</h3>
-           <div className='anything'>
-                <InputG label={'Siorg:'}      disabled={true}   value={this.state.selected.siorg} />
-                <InputG label={'Data'}         value={dado[3]} />
-                <InputG label={'Descricao:'}  value={dado[0]} />
-                <InputG label={'Quantidade:'} value={this.state.selected.quantidade} />
-                <InputG label={'Justificativa:'} value={dado[2]} />
-                <Input type="select" name="select" id="exampleSelect" value={this.state.selected.status} onChange={this.setselect}>
-                    {this.loadSelect()}
-                </Input>
-                {feed}
-                <OrcamentosTable urlGet={"http://localhost:3001/solicitacoes/" + this.props.match.params.id +"/orcamentos"}/>
-                </div>
+           <div id = "Inputs">
+                        <FormGroup>
+                            <Label> Siorg</Label>
+                             <div id='siorgButton'>
+                                <Input label='Siorg:' name='siorg' type='text' id='inputSiorg' disabled='true' value={this.state.selected.siorg}/>
+                                <Button id="buttonSiorg" color="secondary" onClick={this.toggle}>Lista Siorg</Button> 
+                            </div>    
+                        <Modal isOpen={this.state.modal} toggle={this.toggle} className='modal-xl'>
+                          <ModalHeader toggle={this.toggle}>Lista Siorg</ModalHeader>
+                          <ModalBody>
+                             <TableSiorg urlGet={'https://gist.githubusercontent.com/caionakai/1ee2b50876f4ac8fe689b89f35580851/raw/c7909049b0603707ad77c7bc0ea65c857014ca72/siorg.json'}/>
+                          </ModalBody>
+                          <ModalFooter>
+                            <Button color="primary" onClick={this.toggle}>Confirmar</Button>{' '}
+                            <Button color="secondary" onClick={this.toggle}>Cancelar</Button>
+                          </ModalFooter>
+                        </Modal>
+                        </FormGroup>
+                    <InputG label={'Data'}   value={dado[3]} />
+                    <InputG label={'Descrição'}  value={dado[0]} />
+                    <InputG label={'Quantidade'} type={'number'} value={this.state.selected.quantidade} />
+                    <InputG label={'Justificativa'} value={dado[2]} />
+                    <FormGroup>
+                    <Label>Status</Label>
+                    <Input type="select" name="select" id="exampleSelect" value={this.state.selected.status} onChange={this.setselect}>
+                        {this.loadSelect()}
+                    </Input>
+                    </FormGroup>
+                    {feed}
+                    <OrcamentosTable urlGet={"http://localhost:3001/solicitacoes/" + this.props.match.params.id +"/orcamentos"}/>
             </div>
+        </div>
     )
   }
 }
