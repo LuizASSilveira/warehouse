c420f22e0f6f858d09b0a8c7ba396aa0c3436e6d
doouglasabreu <douglasabrel97@gmail.com>
2018-06-14 17:05:28 -0300

    correcao

--- src/componentes/modal-almoxarifado/modal.js
+++ src/componentes/modal-almoxarifado/modal.js
@@ -14,7 +14,7 @@ class ModalAmoxarifado extends React.Component {
 
                         <div id={this.props.divNum}>
                             <Label> {this.props.label}: </Label><br />
-                            <NumericInput type="number" min={1} max={ this.props.max } value={this.props.value} onChange={this.props.setQuantidade} />
+                            <NumericInput type="number" min={1} max={ this.props.max } value={this.props.value} onChange={this.props.func} />
                         </div>
                         <div id={this.props.divID}>
                             <Label check id= "checkModal">
