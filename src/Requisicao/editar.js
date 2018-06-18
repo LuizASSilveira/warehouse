c14bd47ed72d19d5a69e46439ca4d69a1062d2cf
caionakai <kaioesperto@hotmail.com>
2018-06-18 20:30:43 -0300

    modal add solicitacao em editar req
    falta metade

--- src/Requisicao/editar.js
+++ src/Requisicao/editar.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import Nav from '../componentes/navbarAdm';
+import {Table} from '../componentes/table/addSolicitacaoReq';
 import {TableEditar} from '../componentes/table/tableEditarRequisicao';
+import {Input, Button,Label ,FormGroup, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+
 
 export default class Criar extends Component {     
+    constructor(props){
+        super(props)
+        this.state = {
+            modal: false
+        }
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+      this.setState({
+        modal: !this.state.modal
+      });
+    }
+
     render(){
         return(
             <div>
@@ -25,7 +43,37 @@ export default class Criar extends Component {
                         dataL=      {'data'}
                         nomeL=      {'nome'}
                         orcamentoL= {'orcamento'}
-                />                   
+                />
+
+                <Button onClick={this.toggle}> Adicionar Solicitação </Button>
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className='modal-xl'>
+                    <ModalHeader toggle={this.toggle}>Adicionar Solicitação</ModalHeader>
+                    
+                    <ModalBody>
+                        <Table  
+                            func = {this.controlAlert}
+                            history = {this.props.history}
+                            buttonName= {'Criar Requisição'}
+                            urlGet=     {'http://localhost:3001/solicitacoes/requisicao'}
+                            urlPost=    {'http://localhost:3001/requisicoes'}
+                            descricao=  {'Descrição'}
+                            status=     {'Estado'}
+                            data=       {'Data'}
+                            nome=       {'Solicitante'}
+
+                            descricaoL=  {'descricao'}
+                            statusL=     {'status'}
+                            dataL=       {'data'}
+                            nomeL=       {'nome'}
+                        /> 
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button color="primary" >Confirmar</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancelar</Button>
+                    </ModalFooter>
+                </Modal>                
             </div>
         )
     }
---
+++ src/componentes/table/addSolicitacaoReq.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import '../css/table.css'
+import { BootstrapTable, TableHeaderColumn } from '../../../node_modules/react-bootstrap-table';
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import Modal from '../../componentes/confirmModal';
+import { Button, Input, FormGroup, FormFeedback, ModalBody, ModalFooter } from 'reactstrap';
+import '../css/input.css'
+import ExpandTable from './expand'
+
+
+
+
+
+class Table extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { products: [], modal: false, name:'',selected:[], validNome: false, alerta: false}
+    this.toggle = this.toggle.bind(this)
+    this.funcCancel = this.funcCancel.bind(this)
+    this.funcConfirm = this.funcConfirm.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.onRowClick = this.onRowClick.bind(this);
+    this.onRowSelect = this.onRowSelect.bind(this);
+  }
+
+  funcConfirm() {
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({nome: this.state.name ,solicitacoes: this.state.selected}),
+      headers: new Headers({
+        'Content-type': 'application/json',
+        'token': localStorage.getItem('auth-token')
+      })
+    };
+    fetch(this.props.urlPost, requestInfo)
+      .then(response => {
+        if (response.ok) {
+          //alerta dados salvos com sucesso
+          this.props.history.push('/requisicao/historico');
+        } else {
+          throw new Error("não foi possivel salvar as alterações");
+        }
+      })
+    this.funcCancel()  
+}
+
+  funcCancel() {
+    this.setState({modal:false})
+  }
+
+  onRowSelect(row, isSelected) {
+    if (isSelected) {
+      this.setState({selected: this.state.selected.concat(row.id)})
+      this.props.func(false)
+      this.setState({alerta: false})
+    } else {
+      const index = this.state.selected.indexOf(row.id);
+      if(index < 0) return
+      this.state.selected.splice(index, 1)
+      this.setState({selected: this.state.selected})
+      
+    }
+  }
+  
+  toggle() {
+    if(this.state.name.length !== 0 && this.state.selected.length !== 0){
+      this.setState({modal:true, alerta: false})
+    }else{
+      if(this.state.name.length === 0){
+        this.setState({validNome: true});
+      }
+      if (this.state.selected.length === 0) {
+        this.setState({alerta: true})
+        this.props.func(true)
+      }
+    }
+  }
+
+
+  componentDidMount() {
+    fetch(this.props.urlGet, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-type': 'application/json',
+        'token': localStorage.getItem('auth-token')
+      })
+    })
+      .then(response => response.json())
+      .then(product => {
+        this.setState({ products: product });
+      });
+  }
+  
+  handleChange(event) {
+    this.setState({ name: event.target.value, validNome: false });
+  }
+
+  onRowClick(row){
+    this.setState({ id: row.id})
+    this.setState({ redirect: true })
+
+  }
+
+  expandComponent(row) {
+    return (
+      console.log(row),
+      <ExpandTable data={ row } siorg={false} />
+    );
+  }
+
+  isExpandableRow(row) {
+    return true;
+
+  }
+
+  render() {
+    
+    if (this.state.redirect) {
+        this.props.history.push('/solicitacao/validar/'+this.state.id);
+    }
+
+
+
+    const options ={
+      noDataText: 'Não há dados.',
+      onRowDoubleClick: this.onRowClick
+        
+    }
+
+    var selectRowProp = {
+      clickToSelect: true,
+      mode: 'checkbox',
+      bgColor: 'gray',
+      onSelect: this.onRowSelect
+    };
+
+    return (
+      <div>
+      <div id="table">
+        <BootstrapTable
+          data={this.state.products}
+          selectRow={selectRowProp}
+          search={true}
+          pagination
+          hover={true}
+          expandComponent={ this.expandComponent }
+          expandableRow={ this.isExpandableRow }
+          searchPlaceholder='Pesquisar'
+          options={options}
+        >
+          <TableHeaderColumn dataField='id' isKey>  ID                                 </TableHeaderColumn>
+          <TableHeaderColumn width='20%' dataField={this.props.dataL}>      {this.props.data}       </TableHeaderColumn>
+          <TableHeaderColumn width='60%' dataField={this.props.descricaoL}> {this.props.descricao}  </TableHeaderColumn>
+          <TableHeaderColumn width='10%' dataField={this.props.statusL}>    {this.props.status}     </TableHeaderColumn>
+          <TableHeaderColumn width='20%' dataField={this.props.nomeL}>      {this.props.nome}       </TableHeaderColumn>
+        </BootstrapTable>
+    
+      </div>
+      </div>
+    );
+  }
+}
+export { Table }
